3230943f322a11a6d0ca127447af5f4c97d1fe59
Abdullah <amjad88.ir@gmail.com>
2024-10-21 17:57:08 +0300

    Improve admin & code

--- myproject/settings.py
+++ myproject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'employee.apps.EmployeeConfig',
     'accounts.apps.AccountsConfig',
     'wisdom.apps.WisdomConfig',
-    'uploaddata.apps.UploaddataConfig',
     'ckeditor',
     'ckeditor_uploader',
     'django.contrib.admin',
